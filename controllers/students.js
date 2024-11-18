4d7ec52eb831b95eb25f20b722e47cf0ecbbf736
Michael Scharff <minka.scharff@gmail.com>
2024-11-18 17:06:37 +0200

    Updating the createStudents function to get an array of students and create all of them, Changing the getStudent function to finding and deleting and Adding the deleteStudent function

--- controllers/students.js
+++ controllers/students.js
@@ -1,29 +1,49 @@
 const Student = require('../models/student');
-// get's an article ID and deletes the article
-// POST /articles
-// ! request structure
-// ? req.body={keyword, author, title, content, publishedAt:(DATE), source, url:(URL), urlToImage:(URL), ownerId:(ID)}
-module.exports.createStudent = (req, res) => {
-    const { key, name, email } = req.body;
+const { handleError } = require('../errors/ErrorHandler')
 
-    Student.create({ key, name, email })
-        .then((student) => {
-            res.send(student);
-        })
-        .catch((err) => {
-            handleError(err, req, res);
-        });
+module.exports.createStudents = (req, res) => {
+    const { students } = req.body;
+    let isAllOk = true;
+    for (const { key, name, email } of students) {
+        if (email && key) {
+
+        }
+        Student.create({ key, name, email })
+            .catch((err) => {
+                isAllOk = false;
+                handleError(err, req, res);
+            });
+    }
+    res.send({ message: isAllOk ? 'Successfully created' : 'Something wen`t wrong' });
 };
 module.exports.getStudent = (req, res, next) => {
     const { email } = req.params;
 
-    Student.findOne({ email })
+    Student.findOneAndDelete({ email })
         .then((student) => {
             if (!student) {
                 throw new NotFoundError(`No source with this - '${email}' EMAIL, was found.`);
             } else {
-                return res.send(`Your access key is: ${student.key}.`);
+                return res.send({ name: student.name, key: student.key });
             }
         })
         .catch(next);
+};
+
+module.exports.deleteStudent = (req, res) => {
+    const { email } = req.params;
+
+    Student.findOneAndDelete({ email })
+        .then((student) => {
+            if (!student) {
+                throw new NotFoundError(`No key for this ${email} was found.`);
+            } else {
+                return res.send({ message: `Student with this Email '${email}' email was successfully deleted.` });
+            }
+        })
+        .catch((err) => {
+            if (err.name === 'ValidationError') {
+                return res.status(400).send(err);
+            }
+        });
 };
